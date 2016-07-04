7ab4aca6c7b88e08662d0712f46ee7f4b6509202
Matt Bajorek <bajorek.matthew@gmail.com>
2016-07-04 19:36:26 -0400

    Working spotify part without custom search

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 // Require libraries
 var keys = require('./keys');
 var Twitter = require('twitter');
+var spotify = require('spotify');
 var fs = require('fs');
 
 // Initialize twitter client with keys
@@ -13,11 +14,13 @@ var client = new Twitter({
 
 // Get input argument
 var selection = process.argv[2];
+var type = process.argv[3];
 //var selection = process.argv.splice(2, process.argv.length-1).join(' ');
 
 // Switch case to handle input
 switch (selection) {
 	case 'my-tweets': getTweets(); break;
+	case 'spotify-this-song': searchSpotify(); break;
 	default:
 		console.log('');
 		console.log('--------------------------------------------------------------------------------');
@@ -35,11 +38,41 @@ function getTweets() {
 	  	console.log('Liri: Here are your most recent tweets:')
 	  	console.log('--------------------------------------------------------------------------------');
 	  	for (var i=0; i<tweets.length; i++) {
-	  		console.log('')
-	  		console.log('Tweet ' + (i+1) + ':')
+	  		console.log('');
+	  		console.log('Tweet ' + (i+1) + ':');
 	  		console.log(tweets[i].created_at);
 	    	console.log(tweets[i].text);
 	  	}
 	  }
+	});
+}
+
+// Search spotify
+function searchSpotify() {
+	var params = {type:'track', query:"what's my age again"};
+	spotify.search(params, function(err, data) {
+    if (err) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    console.log('');
+	  	console.log('--------------------------------------------------------------------------------');
+	  	console.log('Liri: Here the spotify search results:')
+	  	console.log('--------------------------------------------------------------------------------');
+    var search = data.tracks.items;
+    for (var i=0; i<search.length; i++) {
+	  	console.log('');
+	  	console.log('Result ' + (i+1) + ':');
+	 		console.log('Song title: ' + search[i].name);
+	 		for (var j=0; j<search[i].artists.length; j++) {
+	 			if (search[i].artists.length === 1) {
+	 				console.log('Artist: ' + search[i].artists[j].name);
+	 			} else {
+	 				console.log('Artist ' + (j+1) + ': ' + search[i].artists[j].name);
+	 			}
+	 		}
+	 		console.log('Album title: ' + search[i].album.name);
+	 		console.log('Preview URL: ' + search[i].preview_url);
+	  	}
 	});
 }
